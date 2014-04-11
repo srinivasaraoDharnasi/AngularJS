e67f23e883e317ef737355fe62f0fe11798d7358
srinivasaraoDharnasi <srinivasarao@autohona.com>
2014-04-12 03:38:42 +0530

    first commit

---
+++ Tasks/SearchTableContent/maincontroller.js
@@ -0,0 +1,15 @@
+	app.controller("MainController", function($scope){
+				$scope.people = [
+					{"name":"Srinivasarao","Designation":"software Engineer"},
+					{"name":"Tagore","Designation":"software Engineer"},
+					{"name":"Surya","Designation":"Team Lead"},
+					{"name":"Natraj","Designation":"software Engineer"},
+					{"name":"Sai","Designation":"Proffesor"},
+					{"name":"Sundhar","Designation":"student"},
+					{"name":"Ganesh","Designation":"Postal Department"},
+					{"name":"Tulasi","Designation":"Advocate"},
+					{"name":"Nagarjuna","Designation":"Press"},
+					{"name":"Harsha","Designation":"Advocate"},
+					{"name":"Ashok","Designation":"Team Lead"}
+				]
+			});
