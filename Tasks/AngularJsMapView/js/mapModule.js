368c36c691be3ca85772cbbbbb08efab59cbb219
srinivasaraoDharnasi <srinivasarao@autohona.com>
2014-04-19 03:17:18 +0530

    After Routing

--- Tasks/AngularJsMapView/js/mapModule.js
+++ Tasks/AngularJsMapView/js/mapModule.js
@@ -1,50 +1,21 @@
 app.controller("mapController", function($scope){
-
-$scope.location = '';
+$scope.locationsource = '';
+$scope.locationdestination='';
 	$scope.doSearch = function(){
-		if($scope.location === ''){
-			alert('Directive did not update the location property in parent controller.');
+	
+		if($scope.locationsource === ''){
+			alert('Please Enter Source');
+		}else if($scope.locationdestination === ''){
+				alert('Please Enter Destination');
 		}else {
-			alert('Yay. Location: ' + $scope.location);
+			alert('Source: '+$scope.locationsource	+'Destination: '+$scope.locationdestination);
         }
+		
 	};
-
-			$scope.map = {
-					markerproperties : {
-						lat: 17.48888,
-						lng: 78.8978
-					},
-				center : {
-					latitude : 17.48888,
-					longitude :78.8978
-				},
-				zoom : 10,
-				options : {			       
-				        mapTypeId     : google.maps.MapTypeId.ROADMAP,
-				        mapTypeControl: true,
-				        mapTypeControlOptions: {
-				            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-				            position: google.maps.ControlPosition.BOTTOM_CENTER
-				        },
-				        zoomControl   : true,
-				        zoomControlOptions: {
-				            style: google.maps.ZoomControlStyle.LARGE,
-				            position: google.maps.ControlPosition.RIGHT_TOP
-				        },
-				        scaleControl  : true,
-				        scaleControlOptions: {
-				            position: google.maps.ControlPosition.RIGHT_TOP
-				        },
-				        panControl    : true,
-				        panControlOptions: {
-				            position: google.maps.ControlPosition.RIGHT_TOP
-				        },
-				        streetViewControl: true,
-				        streetViewControlOptions: {
-				            position: google.maps.ControlPosition.RIGHT_TOP
-				        }
-				}
-			}
-
-		});
+});
+		
+		
+		
+		
+		
 		
